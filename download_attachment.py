977c73e584cf186249f1fe697dd5194fdd4e47e3
Tianchu Zhao <evantczhao@gmail.com>
2022-03-20 05:06:46 +0000

    error handling

--- download_attachment.py
+++ download_attachment.py
@@ -7,6 +7,15 @@ with open('file.pkl', 'rb') as file:
     myvar = pickle.load(file)
     print(len(myvar["GLOBAL_absolute_download_url"]))
 
+def download(absolute_download_url, downloaded_file_path, auth, headers, verify_peer_certificate, proxies):
+    try:
+        utils.http_download_binary_file(absolute_download_url, downloaded_file_path, auth, headers, verify_peer_certificate, proxies)
+    except utils.ConfluenceException as e:
+        if error_output:
+            error_print('%sERROR: %s' % ('\t'*(depth+2), e))
+        else:
+            print('%sWARNING: %s' % ('\t'*(depth+2), e))
+
 with Pool(processes=1000) as pool:
     pool.starmap(utils.http_download_binary_file, zip(
         myvar["GLOBAL_absolute_download_url"],
